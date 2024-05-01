4a349a810f4410d6e42d603ca42830e6fb557329
Tanvir Ahmed <61121499+TanvirAhmed1@users.noreply.github.com>
2024-05-01 18:53:01 +0600

    Small Optimizations for API

--- API/Data/UserRepository.cs
+++ API/Data/UserRepository.cs
@@ -1,5 +1,8 @@
-﻿using API.Entities;
+﻿using API.DTOs;
+using API.Entities;
 using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
@@ -7,11 +10,29 @@ namespace API.Data
     public class UserRepository : IUserRepository
     {
         private readonly DataContext context;
+        private readonly IMapper mapper;
 
-        public UserRepository(DataContext context)
+        public UserRepository(DataContext context, IMapper mapper)
         {
             this.context = context;
+            this.mapper = mapper;
         }
+
+        public async Task<MemberDto> GetMemberAsync(string username)
+        {
+            return await context.Users
+                .Where(x => x.UserName == username)
+                .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                .SingleOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<MemberDto>> GetMembersAsync()
+        {
+            return await context.Users
+                .ProjectTo<MemberDto>(mapper.ConfigurationProvider)
+                .ToListAsync();
+        }
+
         public async Task<AppUser> GetUserByIdAsync(int id)
         {
             return await context.Users.FindAsync(id);
